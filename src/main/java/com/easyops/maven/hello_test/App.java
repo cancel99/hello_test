014bb1f2f35d7dbcd6f91969c013c32cee27c369
cancel99 <45496@qq.com>
2016-08-29 22:58:52 +0800

    first commit

---
+++ src/main/java/com/easyops/maven/hello_test/App.java
@@ -0,0 +1,21 @@
+package com.easyops.maven.hello_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.print(returnInfo());
+    }
+    
+    public static String returnInfo()
+    {
+    	String str1 = "Hello";
+    	String str2 = "World";
+    	return str1+str2;
+    }
+    
+}
